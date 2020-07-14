c833dfa001e62c092987aac7065ec6396e865e21
DESKTOP-CS6GGT3\Usuario <L18090219@zacatepec.tecnm.mx>
2020-07-14 10:59:14 -0500

    no message

---
+++ public/funciones/sensores.php
@@ -0,0 +1,14 @@
+<?
+require __DIR__ . '/../../src/models/sensores.php';
+function funcionsensores($request){
+    $objSensor= new Sensores();
+    return $objSensor->insertarSensor($request);
+}
+function funciongetSensoreData($request){
+    $objSensor= new Sensores();
+    return $objSensor->getSensorData($request);
+}
+function funcionUpdate($request){
+    $objSensor= new Sensores();
+    return $objSensor->cambiaSensorValor($request);
+}
